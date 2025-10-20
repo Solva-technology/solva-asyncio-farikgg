f784d2b20a78940b90f4c2516326800c27c2828e
farikgg <farikfarik319@gmail.com>
2025-10-20 09:35:05 +0500

    20.11 9:34 AM

--- tasks/task5.py
+++ tasks/task5.py
@@ -1,31 +1,40 @@
-# ЗАДАЧА 5: Параллельное выполнение и возврат первого результата
-
-# ДАНО:
-# - Несколько задач с разными временами выполнения:
-#     → fast_task (0.1 сек)
-#     → medium_task (0.3 сек)
-#     → slow_task (1 сек)
-
-# ЧТО НУЖНО СДЕЛАТЬ:
-# - Написать функцию `first_complete()`, которая:
-#     → запускает все задачи параллельно
-#     → ждет, пока завершится хотя бы одна из них
-#     → возвращает результат самой первой завершившейся
-
-# ПОДСКАЗКА:
-# Используйте:
-#   `done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)`
 import asyncio
+
+FAST_TIME = 0.1
+MEDIUM_TIME = 0.3
+SLOW_TIME = 1
+
+RESULT_FAST_TASK = 'fast'
+RESULT_MEDIUM_TASK = 'medium'
+RESULT_SLOW_TASK = 'slow'
 
 
 async def fast_task():
-    pass
+    await asyncio.sleep(FAST_TIME)
+    return RESULT_FAST_TASK
+
 
 async def medium_task():
-    pass
+    await asyncio.sleep(MEDIUM_TIME)
+    return RESULT_MEDIUM_TASK
+
 
 async def slow_task():
-    pass
+    await asyncio.sleep(SLOW_TIME)
+    return RESULT_SLOW_TASK
+
 
 async def first_complete():
-    pass
+    tasks = [
+        asyncio.create_task(fast_task()),
+        asyncio.create_task(medium_task()),
+        asyncio.create_task(slow_task())
+    ]
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+    first_result = next(iter(done)).result()
+
+    for task in pending:
+        task.cancel()
+
+    return first_result
--- tests/test_task5.py
+++ tests/test_task5.py
@@ -2,6 +2,7 @@ import pytest
 
 from tasks import first_complete
 
+
 @pytest.mark.asyncio
 async def test_first_complete():
     result = await first_complete()
